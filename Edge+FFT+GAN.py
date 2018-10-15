8d2ad8665b82dfa4a0d7d08e87201b6cd5d0f74b
plawinski <jason.plawinski@gmail.com>
2018-10-15 10:49:33 +0000

    Adding comments to the Network code

--- Edge+FFT+GAN.py
+++ Edge+FFT+GAN.py
@@ -17,7 +17,12 @@ import skimage.transform
 from scipy import ndimage as ndi
 from skimage import feature
 
+###################################
+##### Display functions ###########
+###################################
+
 def PixelUpscale(LRimage):
+'''Funtion for displaying Nearest Neighbors interpolation in the result comparasion image'''
     LRsize = LRimage.shape
     upLRimage = np.zeros([LRsize[0]*4, 4*LRsize[1], LRsize[2]])
     for i in range(4):
@@ -26,6 +31,7 @@ def PixelUpscale(LRimage):
     return(upLRimage)
 
 def CompareImage(upLRimage,Bicubic,Outimage,HRTimage):
+    '''Generate a result image containing Ground Truth, NN interpolation, Bicubic Interpolation and Neural Network Super Resolution'''
     HRsize = upLRimage.shape
     CompareI = np.zeros([HRsize[0], HRsize[1]*4, HRsize[2]])
     CompareI[:,:HRsize[1],:] = upLRimage[:,:,:]
@@ -35,6 +41,8 @@ def CompareImage(upLRimage,Bicubic,Outimage,HRTimage):
     return(CompareI)
 
 def CompareImageDelimit(upLRimage,Bicubic,Outimage,HRTimage):
+'''Generate a result image containing Ground Truth, NN interpolation, Bicubic Interpolation and Neural Network Super Resolution
+but with white separations between the images'''
     HRsize = upLRimage.shape
     CompareI = np.ones([HRsize[0], HRsize[1]*4 + 30, HRsize[2]])
     CompareI[:,:HRsize[1],:] = upLRimage[:,:,:]
@@ -44,9 +52,17 @@ def CompareImageDelimit(upLRimage,Bicubic,Outimage,HRTimage):
     return(CompareI)
 
 def BuildTestImage(LR, Generated, Target, epoch):
+'''Save and create the image using CompareImageDelimit to show training results
+works for up to 9999 EpochLossList
+Name and path of the file have to be input manually and cannot be specified as arguments'''
+    #Nearest Neighbors using custom function
+    #Transpose is needed because the image is saved with PIL image which indexes color channels on 1st component
     upLR = PixelUpscale(LR.numpy().transpose((1,2,0)))
     hrTarget = Target.numpy().transpose((1,2,0))
+    #PostprocessG recenters the image (intially from tanh so between -1 and 1), saturates values that are out of range for 
+    #for display and reorders component
     hrGenerated = PostprocessG(Generated)
+    #Bicubic using skimage
     Bicubic = skimage.transform.rescale(LR.numpy().transpose((1,2,0)),4.0,order=3)
     TestImage = CompareImageDelimit(upLR ,Bicubic, hrGenerated, hrTarget)
     TestImage = Image.fromarray(np.uint8(TestImage*255))
@@ -61,6 +77,7 @@ def BuildTestImage(LR, Generated, Target, epoch):
     TestImage.save(name)
 
 def ShowTestImage(LR, Generated, Target, epoch):
+'''Same as BuildTestImage but does not save the image and only displays the image if in a notebook'''
     upLR = PixelUpscale(LR.numpy().transpose((1,2,0)))
     hrTarget = Target.numpy().transpose((1,2,0))
     hrGenerated = PostprocessG(Generated)
@@ -70,6 +87,8 @@ def ShowTestImage(LR, Generated, Target, epoch):
     plt.show()
 
 def PostprocessEdges(Edges):
+'''Post processes the edges, not used in this iteration but can be used to show Edges or Sobel filtering in the comapre image
+Basically just expands from 1 to 3 component to display with other color images'''
     Edges = Edges.numpy().transpose((1,2,0))
     Edges3Channels = np.zeros([Edges.shape[0], Edges.shape[1], 3])
     Edges3Channels[:,:,0] = Edges[:,:,0]
@@ -78,6 +97,8 @@ def PostprocessEdges(Edges):
     return Edges3Channels
 
 def PostprocessG(Generated):
+'''Post processing of the Neural Net image
+Center from [-1, 1] (from tanh) to [0,1], saturates for dispaly reasons and from pytorch tensor to numpy array'''
     HRTest= Generated.numpy().transpose((1,2,0))
     HRTest= (HRTest + np.ones_like(HRTest))*0.5
     HRTest[HRTest>1.0]=1.0
@@ -85,11 +106,18 @@ def PostprocessG(Generated):
     return(HRTest)
 
 def rgb2gray(rgb):
+'''Classic rgb to gray function'''
     r, g, b = rgb[0,:,:], rgb[1,:,:], rgb[2,:,:]
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
     return gray
 
+###################################
+##### Neural Network Modules ######
+###################################
+
 class opt(object):
+'''Class containing options for training the network
+step is useless as it has been replaced by pytorch scheduler'''
     def __init__(self, batchSize = 10, nEpochs = 1002, lr=1e-4, step=500, threads = 0):
         self.batchSize = batchSize
         self.nEpochs = nEpochs
@@ -98,6 +126,13 @@ class opt(object):
         self.threads = threads
 
 class _Residual_Block(nn.Module):
+'''Definition of Residual Blocks
+Reflection Padding for all convs
+64 channels
+3 kernel size (stride 1)
+Instance Normalisation
+Leaky ReLU 0.2
+'''
     def __init__(self):
         super(_Residual_Block, self).__init__()
         
@@ -113,24 +148,33 @@ class _Residual_Block(nn.Module):
         identity_data = x
         output = self.relu(self.bn1(self.conv1(self.pad1(x))))
         output = self.bn2(self.conv2(self.pad2(x)))
+        #add output of the ResBlock with its input with a skip connection
         output = torch.add(output,identity_data)
         return output
 
 class SobelLoss(nn.Module):
+'''Computation of the 1st order derivative of the image with a Sobel Filter to emphasize edges'''
     def __init__(self):
         super(SobelLoss, self).__init__()
+        #definition of the Sobel filter
         npSobel = np.array([[[[1.0, 0.0, -1.0], [2., 0., -2], [1.0, 0.0, -1.0]]], [[[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]]]],dtype=float)
         sobelTensor = torch.from_numpy(npSobel).float()
+        #Convolution with fixed weights
+        #Outputs the Horizontal and Vertical derivatives as 2 different channels (they will be combined later in the loss function)
         self.SobelFilter = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride =1, padding=1, bias= False)
         self.SobelFilter.weight.data = sobelTensor
     
     def forward(self, x):
+        #Only apply the Sobel filter to the grayscale image (no weighting by color)
         gray = torch.sum(x, dim=1)
         gray = gray.view(-1,1,256,256)
         GradImage = self.SobelFilter(gray)
         return GradImage
 
 class FFTLoss(nn.Module):
+'''Compute the Fourier spectrum of the image to compare frequencies
+It is done by patch so the information is not totally global information
+In this implementation the windows are of size 32x32 (out of a 256x256 image or RandomCrop)'''
     def __init__(self, batchsize):
         super(FFTLoss, self).__init__()
         self.eps = 1e-7
@@ -141,16 +185,20 @@ class FFTLoss(nn.Module):
         x = x.view(-1, 3*64, 32, 32)
         t = torch.cat((x, x2), 1)
         
-       
+       #Extract the FFT using torch algorithm
         vF = torch.rfft(x, 2, onesided = False)
+        #get real part
         vR = vF[:,:,:,0]
+        #get the imaginary part
         vI = vF[:,:,:,1]
         
+        #Get spectrum by computing the elemcent wise complex modulus
         out = torch.add(torch.pow(vR,2), torch.pow(vI,2))
         out = torch.sqrt(out + self.eps)
         return out
     
 class UpscaleNet(nn.Module):
+'''Definition of the Super Resolution Network (Generator in Adversarial training)'''
     def __init__(self):
         super(UpscaleNet, self).__init__()
         
